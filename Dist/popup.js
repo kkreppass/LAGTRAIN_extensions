fc2bb89d5f49434ec3e05735790289caa8bfbf66
Atobe <93640494+Atobe1108@users.noreply.github.com>
2022-04-23 08:52:05 +0900

    Update popup.js

--- Dist/popup.js
+++ Dist/popup.js
@@ -1,12 +1,12 @@
 function a () {
     if(location.href.indexOf('watch') != -1 && location.href != 'https://www.youtube.com/watch?v=UnIhRpIT7nc&t=1s') {
-        window.alert("이 동영상은 적합하지 않습니다! 다른 동영상을 추천해드리겠습니다.");
+        //window.alert("이 동영상은 적합하지 않습니다! 다른 동영상을 추천해드리겠습니다.");
         location.href="https://www.youtube.com/watch?v=UnIhRpIT7nc&t=1s";
         console.log("완료.")
     }}
-    //document.getElementsByTagName("input")[0].setAttribute("placeholder", "유튜브 중딩한테 다털렸쥬?ㅋㅋㅋ")
+   
     
-    setInterval(a, 2000);
+    setInterval(a, 500);
     /*if(location.href.indexOf('watch') != -1 && location.href != 'https://www.youtube.com/watch?v=UnIhRpIT7nc&t=1s') {
         window.alert("이 동영상은 적합하지 않습니다! 다른 동영상을 추천해드리겠습니다.");
         location.href="https://www.youtube.com/watch?v=UnIhRpIT7nc&t=1s";
