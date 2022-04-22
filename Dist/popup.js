7fa030982d5818359a27acc903fa61931a3da451
Atobe <93640494+Atobe1108@users.noreply.github.com>
2022-04-22 22:17:27 +0900

    Update popup.js

--- Dist/popup.js
+++ Dist/popup.js
@@ -1,14 +1,17 @@
-/*document.addEventListener('DOMContentLoaded', function() {
-let checkbox = document.getElementById('switch').checked;
-if(location.href.indexOf('watch') > -1) {
-    while(1) {
-        window.alert("hi");
-    }
-}
-});*/
 
-function a () {if(location.href.indexOf('watch') != -1 && location.href != 'https://www.youtube.com/watch?v=UnIhRpIT7nc&t=1s') {
-location.href="https://www.youtube.com/watch?v=UnIhRpIT7nc&t=1s";
+function a () {
+if(location.href.indexOf('watch') != -1 && location.href != 'https://www.youtube.com/watch?v=UnIhRpIT7nc&t=1s') {
+    window.alert("이 동영상은 적합하지 않습니다! 다른 동영상을 추천해드리겠습니다.");
+    location.href="https://www.youtube.com/watch?v=UnIhRpIT7nc&t=1s";
+    
 }}
+//document.getElementsByTagName("input")[0].setAttribute("placeholder", "유튜브 중딩한테 다털렸쥬?ㅋㅋㅋ")
 
 setInterval(a, 2000);
+/*if(location.href.indexOf('watch') != -1 && location.href != 'https://www.youtube.com/watch?v=UnIhRpIT7nc&t=1s') {
+    window.alert("이 동영상은 적합하지 않습니다! 다른 동영상을 추천해드리겠습니다.");
+    location.href="https://www.youtube.com/watch?v=UnIhRpIT7nc&t=1s";
+    
+    
+}
+console.log("hi");*/
