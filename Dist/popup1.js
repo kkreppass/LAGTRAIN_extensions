163e6a8e876a49be96f5b0275b1c01e7f07543c1
Atobe <93640494+Atobe1108@users.noreply.github.com>
2022-04-23 09:09:45 +0900

    Update popup1.js

--- Dist/popup1.js
+++ Dist/popup1.js
@@ -1,2 +1,2 @@
-function change () {document.getElementsByTagName("input")[0].setAttribute("placeholder", "레그트레인 사랑해주세요!")}
+function change () {document.getElementsByTagName("input")[0].setAttribute("placeholder", "레그트레인 사랑해주세요")}
 setInterval(change, 500);
